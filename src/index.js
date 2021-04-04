ebe64abf6b9b0975abd92c617d2c2cbfaa7119e9
wahed2102 <wahed.khan@rediffmail.com>
2021-04-04 10:35:39 +0530

    Setting Auth0Provider

--- src/index.js
+++ src/index.js
@@ -5,14 +5,25 @@ import App from "./App";
 import { ProductsProvider } from "./context/products_context";
 import { FilterProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+//wahed-test.us.auth0.com
+//JAkFB9ADtGRzDVLU0s3fAjwwI5eZpYv0
 
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain="wahed-test.us.auth0.com"
+    clientId="JAkFB9ADtGRzDVLU0s3fAjwwI5eZpYv0"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>,
   document.getElementById("root")
 );
